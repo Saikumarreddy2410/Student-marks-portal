5561cc86a557e580f2624e084f4ba3de2c9e3f42
Geerisaikumar <saikumargeeri@gmail.com>
2023-12-30 00:15:41 +0530

    remove the unused imports

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App, { appRouter } from "./App";
+import { appRouter } from "./App";
 import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
